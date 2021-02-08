739f1022bca7e8ee4820ae9fe8629b2acd4716e1
Matt <08martinm@gmail.com>
2021-02-08 12:44:20 -0800

    Removes extra prettier/eslint configs, updates eslint config

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,12 +3,12 @@ import { Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => (
-    <Alert variant="success">
-      You made it to the dashboard!{' '}
-      <Alert.Link>
-        <Link to="/">Go back</Link>
-      </Alert.Link>
-    </Alert>
-  );
+  <Alert variant="success">
+    You made it to the dashboard!{' '}
+    <Alert.Link>
+      <Link to="/">Go back</Link>
+    </Alert.Link>
+  </Alert>
+);
 
 export default Dashboard;
